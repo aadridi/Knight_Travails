452ed3468909a7a4b39726875dffea8b659326ba
Akrame-Amine Dridi <aadridi@gmail.com>
2025-12-02 00:27:25 +0100

    Create Square (class), populate Board & knightMove

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('Hello World!');
+
+class Square {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.visited = false;
+		this.parent = null;
+	}
+}
+
+// Creating board and populate it with Squares
+const board = [];
+
+for (let i = 0; i < 8; i++) {
+	board[i] = [];
+
+	for (let j = 0; j < 8; j++) {
+		board[i][j] = new Square(i, j);
+	}
+}
+
+function knightMoves(x, y) {
+	const moves = [
+		[x + 2, y + 1],
+		[x + 2, y - 1],
+		[x - 2, y + 1],
+		[x - 2, y - 1],
+		[x + 1, y + 2],
+		[x + 1, y - 2],
+		[x - 1, y + 2],
+		[x - 1, y - 2],
+	];
+
+	// filtrer les mouvements hors plateau
+	return moves.filter(([nx, ny]) => nx >= 0 && nx < 8 && ny >= 0 && ny < 8);
+}
+
+function findShortestPath(start, end) {
+    // TBD
+}
